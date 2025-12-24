e1c6c962f0a1e7d1fc96e5ebc9598608bb7e6f6a
1999raju <rajusoora123@gmail.com>
2025-12-24 09:45:57 +0530

    Update orders.js

--- frontend/js/orders.js
+++ frontend/js/orders.js
@@ -1,14 +1,9 @@
-fetch('http://localhost:5000/orders')
-.then(res => res.json())
-.then(data => {
-const div = document.getElementById('orders');
-if (data.length === 0) {
-div.innerHTML = '<p>No orders yet</p>';
+function placeOrder() {
+  if(cart.length === 0) {
+    alert("Cart is empty!");
+    return;
+  }
+  alert("Order placed successfully!");
+  cart = [];
+  document.querySelector(".cart").innerText = "🛒 Cart";
 }
-data.forEach(order => {
-div.innerHTML += `<div class="card">
-<p>Order Date: ${new Date(order.date).toLocaleString()}</p>
-<p>Items: ${order.items.length}</p>
-</div>`;
-});
-});
