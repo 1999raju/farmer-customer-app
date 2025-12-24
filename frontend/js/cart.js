a1e5b191d350e8022d0ae6127e1725f897d836b2
1999raju <rajusoora123@gmail.com>
2025-12-24 09:45:35 +0530

    Update cart.js

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -1,12 +1,6 @@
-function placeOrder() {
-const cart = JSON.parse(localStorage.getItem('cart')) || [];
-fetch('http://localhost:5000/order', {
-method: 'POST',
-headers: { 'Content-Type': 'application/json' },
-body: JSON.stringify({ items: cart, date: new Date() })
-})
-.then(() => {
-alert('Order placed');
-localStorage.removeItem('cart');
-});
+let cart = [];
+
+function addToCart(name, price) {
+  cart.push({ name, price });
+  document.querySelector(".cart").innerText = `🛒 Cart (${cart.length})`;
 }
